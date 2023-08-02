3a920b80237d4ef426b6c88615cf69ea29e6608c
David <66641216+fhswno@users.noreply.github.com>
2023-08-02 23:57:58 +0100

    DataCharts now supports bar charts, pie charts and line charts for the date of birth, salary and years of experience fields. Data is grouped in relevant ranges, and the user can select the preferred visualisation. Null values are removed.

---
+++ src/components/chart/DataCharts.jsx
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  PieChart,
+  Pie,
+  Cell,
+  LineChart,
+  Line,
+} from "recharts";
+import moment from "moment";
+import _ from "lodash";
+import mock_data from "../../MOCK_DATA.json";
+import "./DataCharts.css";
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+export const DataCharts = () => {
+  const [chartType, setChartType] = useState("BarChart");
+  const [dataProperty, setDataProperty] = useState("date_of_birth");
+
+  const getSalaryRange = (salary) => {
+    if (salary === null) {
+      return null;
+    }
+
+    const base = Math.floor(salary / 10000);
+    return `${base * 10}k - ${base * 10 + 10}k`;
+  };
+
+  const getExperienceRange = (experience) => {
+    if (experience === null) {
+      return null;
+    }
+
+    // ranges of one year
+    const base = Math.floor(experience);
+    return `${base} - ${base + 1}y`;
+  };
+
+  const transformedData = mock_data
+    .filter(
+      (item) =>
+        item.salary !== null &&
+        item.years_of_experience !== null &&
+        !isNaN(moment(item.date_of_birth).year())
+    )
+    .map((item) => ({
+      ...item,
+      birthYear: moment(item.date_of_birth).year(),
+      salaryRange: getSalaryRange(item.salary),
+      experienceRange: getExperienceRange(item.years_of_experience),
+    }));
+
+  const dataGroupedByProperty = _.groupBy(
+    transformedData,
+    dataProperty === "salary"
+      ? "salaryRange"
+      : dataProperty === "date_of_birth"
+      ? "birthYear"
+      : dataProperty === "years_of_experience"
+      ? "experienceRange"
+      : dataProperty
+  );
+
+  const chartData = Object.keys(dataGroupedByProperty).map((key) => ({
+    name: key,
+    value: dataGroupedByProperty[key].length,
+  }));
+
+  if (dataProperty === "salary") {
+    chartData.sort((a, b) => {
+      const rangeStartA = Number(a.name.split(" ")[0]);
+      const rangeStartB = Number(b.name.split(" ")[0]);
+      return rangeStartA - rangeStartB;
+    });
+  }
+
+  let chart;
+
+  switch (chartType) {
+    case "BarChart":
+      chart = (
+        <BarChart width={600} height={300} data={chartData}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="value" fill="#8884d8" />
+        </BarChart>
+      );
+      break;
+    case "PieChart":
+      chart = (
+        <PieChart width={400} height={400}>
+          <Pie
+            data={chartData}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {chartData.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      );
+      break;
+    case "LineChart":
+      chart = (
+        <LineChart width={600} height={300} data={chartData}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="value" stroke="#8884d8" />
+        </LineChart>
+      );
+      break;
+    default:
+      chart = null;
+  }
+
+  return (
+    <div className="container">
+      <div className="controls">
+        <label htmlFor="chartType">Chart Type:</label>
+        <select
+          id="chartType"
+          value={chartType}
+          onChange={(e) => setChartType(e.target.value)}
+          className="select"
+        >
+          <option value="BarChart">Bar Chart</option>
+          <option value="PieChart">Pie Chart</option>
+          <option value="LineChart">Line Chart</option>
+        </select>
+
+        <label htmlFor="dataProperty">Data Property:</label>
+        <select
+          id="dataProperty"
+          value={dataProperty}
+          onChange={(e) => setDataProperty(e.target.value)}
+          className="select"
+        >
+          <option value="birthYear">Birth Year</option>
+          <option value="years_of_experience">Years of Experience</option>
+          <option value="salary">Salary</option>
+        </select>
+      </div>
+
+      <div className="chart-container">{chart}</div>
+    </div>
+  );
+};
